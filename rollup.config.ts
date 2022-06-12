043dd209e3f4322abfa09cdd5514b182b78abfe8
taolt <taoliangtong@qq.com>
2022-06-12 21:53:52 +0800

    feat: init

---
+++ rollup.config.ts
@@ -0,0 +1,45 @@
+import { cleandir } from 'rollup-plugin-cleandir'
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import bable from '@rollup/plugin-babel'
+import { DEFAULT_EXTENSIONS } from '@babel/core'
+
+import { defineConfig } from 'rollup'
+
+const rollupConfig = defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    cleandir(),
+    // 支持处理 typescript 文件
+    typescript(),
+    // 将 commonjs 转换为 ES 模块
+    commonjs(),
+    // 解析 node_modules 中的模块
+    nodeResolve({
+      preferBuiltins: true,
+      moduleDirectories: ['node_modules']
+    }),
+    bable({
+      babelHelpers: 'runtime',
+      // 只转换源代码，忽略node依赖
+      exclude: 'node_modules/**',
+      // rollup-babel 默认不转换 ts 需要手动添加
+      extensions: [...DEFAULT_EXTENSIONS, '.ts']
+    })
+  ]
+})
+
+export default rollupConfig
---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { PluginImpl } from 'rollup'
+import { resolve, relative, sep } from 'path'
+import { tmpdir } from 'os'
+import { readFileSync, writeFileSync } from 'fs'
+import * as readline from 'readline'
+import chalk from 'chalk'
+
+export interface Options {
+  clearLine?: boolean
+}
+
+function normalizePath(id: string) {
+  return relative(process.cwd(), id).split(sep).join('/')
+}
+
+const progress: PluginImpl<Options> = (options?: Options) => {
+  if (options === undefined || options.clearLine === undefined) {
+    options = { clearLine: true }
+  }
+
+  let total = 0
+  const totalFilePath = resolve(tmpdir(), './xxteam-rollup-plugin-progress')
+  try {
+    total = parseInt(readFileSync(totalFilePath).toString(), 10)
+  } catch (e) {
+    writeFileSync(totalFilePath, '0')
+  }
+  const state = {
+    total,
+    loaded: 0
+  }
+
+  return {
+    name: 'progress',
+    load() {
+      state.loaded += 1
+    },
+    transform(_code, id) {
+      const file = normalizePath(id)
+      if (file.includes(':')) {
+        return
+      }
+
+      if (options!.clearLine && process.stdout.isTTY) {
+        readline.cursorTo(process.stdout, 0)
+        let output = ''
+        if (state.total > 0) {
+          const percent = Math.round((100 * state.loaded) / state.total)
+          output += `${Math.min(100, percent)}% `
+        }
+        output += `(${chalk.red(state.loaded)}): ${file}`
+        if (output.length < process.stdout.columns) {
+          process.stdout.write(output)
+        } else {
+          process.stdout.write(output.substring(0, process.stdout.columns - 1))
+        }
+      } else {
+        console.log(`(${chalk.red(state.loaded)}): ${file}`)
+      }
+    },
+    generateBundle() {
+      writeFileSync(totalFilePath, String(state.loaded))
+      if (options!.clearLine && process.stdout.isTTY) {
+        readline.cursorTo(process.stdout, 0)
+      }
+    }
+  }
+}
+
+export default progress
